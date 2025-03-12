0f1df11faa6dc69697b0fdccdbeeb769ff4e3936
Harenabs21 <hei.harena.6@gmail.com>
2025-03-12 21:52:30 +0300

    import the tab bar in the layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,26 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import TabBar from '@/components/TabBar';
+import { BlurView } from 'expo-blur';
+import { StyleSheet } from 'react-native';
 
 export default function TabLayout() {
 
   return (
-    <Tabs>
+    <Tabs tabBar={(props) => <TabBar {...props}/>} 
+      screenOptions={{headerShown: false,
+        tabBarBackground: () => (
+          <BlurView
+            intensity={80}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              overflow: 'hidden',
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}
+          />
+        ),
+      }}>
       <Tabs.Screen name="index" options={{title: 'Home'}}/>
       <Tabs.Screen name="favorites" options={{title: 'Favorites'}}/>
       <Tabs.Screen name="songs" options={{title: 'Songs'}}/>
