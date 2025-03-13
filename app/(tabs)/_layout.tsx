3e72ff791ec1f45825fa93cd1b625fa950139ba5
Harenabs21 <hei.harena.6@gmail.com>
2025-03-13 12:27:05 +0300

    chore: remove BlurView

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,12 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import TabBar from '@/components/TabBar';
-import { BlurView } from 'expo-blur';
-import { StyleSheet } from 'react-native';
 
 export default function TabLayout() {
 
   return (
     <Tabs tabBar={(props) => <TabBar {...props}/>} 
-      screenOptions={{headerShown: false,
-        tabBarBackground: () => (
-          <BlurView
-            intensity={80}
-            style={{
-              ...StyleSheet.absoluteFillObject,
-              overflow: 'hidden',
-              borderTopLeftRadius: 20,
-              borderTopRightRadius: 20,
-            }}
-          />
-        ),
-      }}>
+      screenOptions={{headerShown: false}}>
       <Tabs.Screen name="index" options={{title: 'Home'}}/>
       <Tabs.Screen name="favorites" options={{title: 'Favorites'}}/>
       <Tabs.Screen name="songs" options={{title: 'Songs'}}/>
--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,12 +1,11 @@
 import { View } from 'react-native';
-import { useLinkBuilder, useTheme } from '@react-navigation/native';
+import { useLinkBuilder } from '@react-navigation/native';
 import { Text, PlatformPressable } from '@react-navigation/elements';
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { TabBarStyle } from '@/styles/tab-bar.style';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { MyColors } from '@/constants/Colors';
-
 
 
 export default function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
@@ -22,60 +21,60 @@ export default function TabBar({ state, descriptors, navigation }: BottomTabBarP
 
   return (
     <View style={TabBarStyle.tabBarContainer}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-              ? options.title
-              : route.name;
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+                ? options.title
+                : route.name;
 
-        const isFocused = state.index === index;
+          const isFocused = state.index === index;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, route.params);
-          }
-        };
-
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
-
-        return (
-          <PlatformPressable
-            key={route.name}
-            href={buildHref(route.name, route.params)}
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarButtonTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={TabBarStyle.tabBarItem}
-          >
-        
-           {
-            TabIcons[route.name as keyof typeof TabIcons]({
-                color: isFocused ? MyColors.primary : MyColors.textMuted,
-                })
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, route.params);
             }
-            <Text style={{ color: isFocused ? MyColors.primary : MyColors.textMuted }}>
-                {typeof label === 'function' ? label({ focused: isFocused, color: isFocused ? MyColors.primary : MyColors.textMuted, position:'below-icon', children: '' }) : label}
-            </Text>
-          </PlatformPressable>
-        );
-      })
-    }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return (
+            <PlatformPressable
+              key={route.name}
+              href={buildHref(route.name, route.params)}
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarButtonTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={TabBarStyle.tabBarItem}
+            >
+          
+            {
+              TabIcons[route.name as keyof typeof TabIcons]({
+                  color: isFocused ? MyColors.primary : MyColors.textMuted,
+                  })
+              }
+              <Text style={{ color: isFocused ? MyColors.primary : MyColors.textMuted }}>
+                  {typeof label === 'function' ? label({ focused: isFocused, color: isFocused ? MyColors.primary : MyColors.textMuted, position:'below-icon', children: '' }) : label}
+              </Text>
+            </PlatformPressable>
+          );
+        })
+      }
     </View>
   );
 }
