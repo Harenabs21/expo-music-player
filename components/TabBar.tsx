5b4b466b556ca09bf0e6780b5c587b0e40a80774
Harenabs21 <hei.harena.6@gmail.com>
2025-03-13 16:20:31 +0300

    import TabBarButton component

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,15 +1,10 @@
 import { View } from 'react-native';
-import { useLinkBuilder } from '@react-navigation/native';
-import { Text, PlatformPressable } from '@react-navigation/elements';
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { TabBarStyle } from '@/styles/tab-bar.style';
-import { TabButtonIcons } from '@/constants/route-icon';
-import { MyColors } from '@/constants/Colors';
+import TabBarButton from './TabBarButton';
 
 
 export default function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
-    
-  const { buildHref } = useLinkBuilder();
 
   return (
     <View style={TabBarStyle.tabBarContainer}>
@@ -44,28 +39,14 @@ export default function TabBar({ state, descriptors, navigation }: BottomTabBarP
           };
 
           return (
-            <PlatformPressable
+            <TabBarButton
               key={route.name}
-              href={buildHref(route.name, route.params)}
-              accessibilityState={isFocused ? { selected: true } : {}}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarButtonTestID}
               onPress={onPress}
               onLongPress={onLongPress}
-              style={TabBarStyle.tabBarItem}
-            >
-          
-              {
-              TabButtonIcons[route.name as keyof typeof TabButtonIcons]({
-                  color: isFocused ? MyColors.primary : MyColors.textMuted,
-                  })
-              }
-              {isFocused && (
-                <Text style={{ color: isFocused ? MyColors.primary : MyColors.textMuted }}>
-                    {typeof label === 'function' ? label({ focused: isFocused, color: isFocused ? MyColors.primary : MyColors.textMuted, position:'below-icon', children: '' }) : label}
-                </Text>
-              )}
-            </PlatformPressable>
+              routeName={route.name}
+              isFocused={isFocused}
+              label={label}
+            />
           );
         })
       }
