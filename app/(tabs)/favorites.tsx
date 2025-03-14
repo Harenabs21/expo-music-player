ae0b53410576feee958ec22ee7c7ac15cb04e421
Harenabs21 <hei.harena.6@gmail.com>
2025-03-14 18:29:51 +0300

    import and use ButtonWithIcon component

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,10 +1,16 @@
+import ButtonWithIcon from '@/components/buttons/ButtonWithIcon';
 import EmptyFavoriteList from '@/components/favorites/EmptyFavoriteList';
-import { StyleSheet, View } from 'react-native';
+import { buttonWithIconStyle } from '@/styles/button-icon.style';
+import { Alert, StyleSheet, View } from 'react-native';
 
 export default function FavoriteScreen() {
+  const handlePlayButton = () => {
+    Alert.alert('you cliked the play button');
+  };
   return (
     <View style={styles.container}>
       <EmptyFavoriteList />
+      <ButtonWithIcon onPress={handlePlayButton} iconName="play" color="#fefffe" style={buttonWithIconStyle.button} />
     </View>
   );
 }
