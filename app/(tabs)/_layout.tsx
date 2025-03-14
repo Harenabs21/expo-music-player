64ac7b8e46460d048960f2afd5c8f4c638f41ea7
Harenabs21 <hei.harena.6@gmail.com>
2025-03-14 14:30:12 +0300

    chore: change header style and header title style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,13 @@ import TabBar from '@/components/TabBar';
 
 export default function TabLayout() {
   return (
-    <Tabs tabBar={(props) => <TabBar {...props} />} screenOptions={{ headerShown: false }}>
+    <Tabs
+      tabBar={(props) => <TabBar {...props} />}
+      screenOptions={{
+        headerStyle: { elevation: 0, shadowOpacity: 0, backgroundColor: '#fefffe' },
+        headerTitleStyle: { fontSize: 24, fontWeight: 'bold' },
+      }}
+    >
       <Tabs.Screen name="index" options={{ title: 'Home' }} />
       <Tabs.Screen name="favorites" options={{ title: 'Favorites' }} />
       <Tabs.Screen name="songs" options={{ title: 'Songs' }} />
