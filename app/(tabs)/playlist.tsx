81b53c8c3924dffb4fbf568fc4dc4230087df1a4
Harenabs21 <hei.harena.6@gmail.com>
2025-03-17 12:47:47 +0300

    feat: integrate the modal for adding playlist

--- app/(tabs)/playlist.tsx
+++ app/(tabs)/playlist.tsx
@@ -1,16 +1,28 @@
 import ButtonWithIcon from '@/components/buttons/ButtonWithIcon';
+import CreatePlaylistModal from '@/components/playlist/CreatePlayListModal';
 import EmptyPlaylist from '@/components/playlist/EmptyPlaylist';
 import { buttonWithIconStyle } from '@/styles/button-icon.style';
+import { useState } from 'react';
 import { Alert, StyleSheet, View } from 'react-native';
 
 export default function PlaylistScreen() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleCreatePlaylist = (name: string) => {
+    Alert.alert('Playlist created with name:', name);
+  };
   const handleAddButton = () => {
-    Alert.alert('you cliked the add button');
+    setIsModalVisible(true);
   };
   return (
     <View style={styles.container}>
       <EmptyPlaylist />
       <ButtonWithIcon onPress={handleAddButton} iconName="plus" color="#fefffe" style={buttonWithIconStyle.button} />
+      <CreatePlaylistModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+        onSubmit={handleCreatePlaylist}
+      />
     </View>
   );
 }
