76189cbbc64c0f5dde72c1a7cc7c3295490c1083
Harenabs21 <hei.harena.6@gmail.com>
2025-03-18 00:01:07 +0300

    create new route: player

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,25 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import TabBar from '@/components/TabBar';
+import { View } from 'react-native';
+import { FloatingPlayer } from '@/components/FloatingPlayer';
 
 export default function TabLayout() {
   return (
-    <Tabs
-      tabBar={(props) => <TabBar {...props} />}
-      screenOptions={{
-        headerStyle: { elevation: 0, shadowOpacity: 0, backgroundColor: '#fefffe' },
-        headerTitleStyle: { fontSize: 24, fontWeight: 'bold' },
-      }}
-    >
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="favorites" options={{ title: 'Favorites' }} />
-      <Tabs.Screen name="songs" options={{ title: 'Songs' }} />
-      <Tabs.Screen name="playlist" options={{ title: 'Playlists' }} />
-    </Tabs>
+    <View>
+      <Tabs
+        tabBar={(props) => <TabBar {...props} />}
+        screenOptions={{
+          headerStyle: { elevation: 0, shadowOpacity: 0, backgroundColor: '#fefffe' },
+          headerTitleStyle: { fontSize: 24, fontWeight: 'bold' },
+        }}
+      >
+        <Tabs.Screen name="index" options={{ title: 'Home' }} />
+        <Tabs.Screen name="favorites" options={{ title: 'Favorites' }} />
+        <Tabs.Screen name="songs" options={{ title: 'Songs' }} />
+        <Tabs.Screen name="playlist" options={{ title: 'Playlists' }} />
+      </Tabs>
+      <FloatingPlayer />
+    </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,46 @@
-import { useFonts } from 'expo-font';
+import { playbackService } from '@/constants/playback-service';
+import { useSetupTrackPlayer } from '@/hooks/useSetUpTrackPlayer';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useCallback } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import TrackPlayer from 'react-native-track-player';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+TrackPlayer.registerPlaybackService(() => playbackService);
+
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const handleTrackPlayerLoaded = useCallback(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  useSetupTrackPlayer({
+    onLoad: handleTrackPlayerLoaded,
   });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <GestureHandlerRootView>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="player"
+            options={{
+              headerShown: false,
+              presentation: 'card',
+              gestureEnabled: true,
+              gestureDirection: 'vertical',
+              animationDuration: 400,
+            }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
