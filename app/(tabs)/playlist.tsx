55dfe40199e4cff590b301fa8922ad6e4921a177
Harenabs21 <hei.harena.6@gmail.com>
2025-03-18 11:48:10 +0300

    import playlist list in playlist screen

--- app/(tabs)/playlist.tsx
+++ app/(tabs)/playlist.tsx
@@ -1,12 +1,37 @@
 import ButtonWithIcon from '@/components/buttons/ButtonWithIcon';
 import CreatePlaylistModal from '@/components/playlist/CreatePlayListModal';
-import EmptyPlaylist from '@/components/playlist/EmptyPlaylist';
+import { defaultStyles } from '@/styles/default-style.style';
+import { PlaylistsList } from '@/components/playlist/PlaylistsList';
 import { buttonWithIconStyle } from '@/styles/button-icon.style';
-import { useState } from 'react';
-import { Alert, StyleSheet, View } from 'react-native';
+import { useMemo, useState } from 'react';
+import { ScrollView } from 'react-native';
+import { Alert, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useNavigationSearch } from '@/hooks/useNavigationSearch';
+import { usePlaylists } from '@/stores/library.store';
+import { Playlist } from '@/helpers/types';
+import { playlistNameFilter } from '@/helpers/filter';
 
 export default function PlaylistScreen() {
   const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const router = useRouter();
+
+  const search = useNavigationSearch({
+    searchBarOptions: {
+      placeholder: 'Find in playlists',
+    },
+  });
+
+  const { playlists, addToPlaylist } = usePlaylists();
+
+  const filteredPlaylists = useMemo(() => {
+    return playlists.filter(playlistNameFilter(search));
+  }, [playlists, search]);
+
+  const handlePlaylistPress = (playlist: Playlist) => {
+    router.push(`/(tabs)/playlist`);
+  };
 
   const handleCreatePlaylist = (name: string) => {
     Alert.alert('Playlist created with name:', name);
@@ -15,8 +40,15 @@ export default function PlaylistScreen() {
     setIsModalVisible(true);
   };
   return (
-    <View style={styles.container}>
-      <EmptyPlaylist />
+    <View style={defaultStyles.container}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={{
+          paddingHorizontal: 24,
+        }}
+      >
+        <PlaylistsList scrollEnabled={false} playlists={filteredPlaylists} onPlaylistPress={handlePlaylistPress} />
+      </ScrollView>
       <ButtonWithIcon onPress={handleAddButton} iconName="plus" color="#fefffe" style={buttonWithIconStyle.button} />
       <CreatePlaylistModal
         visible={isModalVisible}
@@ -26,17 +58,3 @@ export default function PlaylistScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fefffe',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-});
